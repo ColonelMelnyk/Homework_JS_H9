6b22cc614bde2d72c14c052f25a11f5a19a8ea29
ColonelMelnyk <119069975+ColonelMelnyk@users.noreply.github.com>
2023-05-09 21:54:08 +0000

    Deploying to gh-pages from @ ColonelMelnyk/Homework_JS_H9@e1bbc56eea22c8eafa436c93009d1d3cc1348340 🚀

---
+++ 01-color-switcher.7fbde234.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click");
+//# sourceMappingURL=01-color-switcher.7fbde234.js.map
---
+++ 01-color-switcher.ca225805.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click")}();
+//# sourceMappingURL=01-color-switcher.ca225805.js.map
