768df2a88cec40068fa51325402a2f21795ba0a1
ColonelMelnyk <xiaomiredmia15@gmail.com>
2023-05-10 11:51:40 +0300

    tasl 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,18 @@
-function getRandomHexColor() {
+ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
-btnStart.addEventListener("click",);
+const body = document.querySelector('body');
+let colorTimer = null;
+btnStart.addEventListener("click", colorChanger);
+btnStop.addEventListener("click", colorChangerStop)
+function colorChanger(){
+  getRandomHexColor();
+ colorTimer = setInterval(()=>{ 
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+}, 1000);
+}
+function colorChangerStop(){
+  clearInterval(colorTimer);
+}
